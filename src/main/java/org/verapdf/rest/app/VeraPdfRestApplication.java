ae838db72fd6a3b11e9c9399f61ff81e8f988a10
Carl Wilson <carl@openpreservation.org>
2016-02-29 13:39:32 +0300

    Added the REST app and config classes.

---
+++ src/main/java/org/verapdf/rest/app/VeraPdfRestApplication.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package org.verapdf.rest.app;
+
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.dropwizard.views.ViewBundle;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.verapdf.rest.resources.ApiResource;
+
+import com.yunspace.dropwizard.xml.XmlBundle;
+
+/**
+ * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
+ *
+ */
+public class VeraPdfRestApplication extends Application<VeraPdfRestConfiguration> {
+
+    private static final String NAME = "verapdf-rest"; //$NON-NLS-1$
+
+    /**
+     * Main method for Jetty server application. Simply calls the run method
+     * with command line args.
+     * 
+     * @param args
+     *            command line arguments as string array.
+     * @throws Exception
+     *             passes any exception thrown by run
+     */
+    public static void main(String[] args) throws Exception {
+        new VeraPdfRestApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public void initialize(Bootstrap<VeraPdfRestConfiguration> bootstrap) {
+        // Dropwizard bundle to handle Jackson XML serialisation
+        final XmlBundle xmlBundle = new XmlBundle();
+        bootstrap.addBundle(xmlBundle);
+        // Allow us to serve views
+        bootstrap.addBundle(new ViewBundle<>());
+        // Allow us to serve static assets for CSS and Javascript
+        bootstrap.addBundle(new AssetsBundle("/assets/css", "/css", null, "css")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        bootstrap.addBundle(new AssetsBundle("/assets/js", "/js", null, "js")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        bootstrap.addBundle(new AssetsBundle("/assets/img", "/img", null, "img")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    }
+
+    @Override
+    public void run(VeraPdfRestConfiguration configuration,
+            Environment environment) throws Exception {
+        // Create & register our REST resources
+        final ApiResource restApi = new ApiResource();
+        environment.jersey().register(restApi);
+        // Set up cross domain REST
+        setupCORS(environment);
+    }
+
+    private static void setupCORS(Environment environment) {
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors = environment.servlets()
+                .addFilter("CORS", CrossOriginFilter.class); //$NON-NLS-1$
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*"); //$NON-NLS-1$ //$NON-NLS-2$
+        cors.setInitParameter(
+                "allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin"); //$NON-NLS-1$ //$NON-NLS-2$
+        cors.setInitParameter(
+                "allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD"); //$NON-NLS-1$ //$NON-NLS-2$
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class),
+                true, "/*"); //$NON-NLS-1$
+    }
+
+}
---
+++ src/main/java/org/verapdf/rest/app/VeraPdfRestConfiguration.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.verapdf.rest.app;
+
+import io.dropwizard.Configuration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Configuration object for the Dropwizard app. Reads defaults from
+ * configuration YAML file. This class has to be "mutable" due to Dropwizard
+ * requirements.
+ * 
+ * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>.</p>
+ */
+public class VeraPdfRestConfiguration extends Configuration {
+    //
+    // FIXME: This isn't mapping to the port option from the config
+    //
+    private int port;
+
+    /**
+     * @return the TCP/IP port used
+     */
+    @JsonProperty
+    public int getPort() {
+        return this.port;
+    }
+
+    /**
+     * @param port
+     *            numeric value of TCP/IP port to listen to
+     */
+    @JsonProperty
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+}
