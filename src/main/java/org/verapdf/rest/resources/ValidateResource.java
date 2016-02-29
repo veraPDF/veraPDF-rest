4ea4406bca956c7a244e692d008734952b8abc77
Carl Wilson <carl@openplanetsfoundation.org>
2016-02-29 15:03:47 +0000

    Added validation resource.

---
+++ src/main/java/org/verapdf/rest/resources/ValidateResource.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.verapdf.rest.resources;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.verapdf.core.ValidationException;
+import org.verapdf.model.ModelParser;
+import org.verapdf.pdfa.PDFAValidator;
+import org.verapdf.pdfa.flavours.PDFAFlavour;
+import org.verapdf.pdfa.results.ValidationResult;
+import org.verapdf.pdfa.validators.Validators;
+
+/**
+ * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
+ *
+ */
+public class ValidateResource {
+    /**
+     * @return a validation profile selected by id
+     * 
+     */
+    @POST
+    @Path("/{profileid}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public ValidationResult validate(
+            @PathParam("profileid") String profileId,
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") final FormDataContentDisposition contentDispositionHeader) {
+        PDFAFlavour flavour = PDFAFlavour.byFlavourId(profileId);
+        try (ModelParser toValidate = new ModelParser(uploadedInputStream)) {
+            PDFAValidator validator = Validators.createValidator(flavour,
+                    Boolean.FALSE);
+            return validator.validate(toValidate);
+        } catch (ValidationException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
