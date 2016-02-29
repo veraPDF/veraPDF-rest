5520ac8db6e7fc7867e69993b922ab2d873198e9
Carl Wilson <carl@openplanetsfoundation.org>
2016-02-29 11:18:18 +0000

    Added Environment information to root.

--- src/main/java/org/verapdf/rest/resources/ApiResource.java
+++ src/main/java/org/verapdf/rest/resources/ApiResource.java
@@ -3,7 +3,13 @@
  */
 package org.verapdf.rest.resources;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.verapdf.rest.environment.Environment;
+import org.verapdf.rest.environment.Environments;
 
 /**
  * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>.</p>
@@ -11,6 +17,13 @@ import javax.ws.rs.Path;
  */
 @Path("/api")
 public final class ApiResource {
+    @GET
+    @Path("/")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Environment getEnvironent() {
+        return Environments.getEnvironment();
+    }
+    
     @Path("/profiles")
     public ProfileResource getProfileResource() {
         return new ProfileResource();
