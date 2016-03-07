f8184401dde9b22ac627ed4d1bce64b5cbfc6778
Timur Kamalov <timur.kamalov@duallab.com>
2016-03-07 12:59:16 +0300

    Moved home page from /api to /home.

---
+++ src/main/java/org/verapdf/rest/resources/HomePageResource.java
@@ -0,0 +1,19 @@
+package org.verapdf.rest.resources;
+
+import org.verapdf.rest.views.RestClientView;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/home")
+public class HomePageResource {
+
+    @GET
+    @Produces({ MediaType.TEXT_HTML })
+    public RestClientView client() {
+        return new RestClientView();
+    }
+
+}
