41e318aa02a8b144e71eb5399ff708fdc65fd43a
georgetarazevich <georgetarazevich@users.noreply.github.com>
2023-11-27 23:28:53 +0300

    Update setup_suite.bash

--- tests/Options/setup_suite.bash
+++ tests/Options/setup_suite.bash
@@ -3,13 +3,11 @@
 setup_suite() {
   echo -e "Starting tests ... \n" >&3
   export DOCKER_ID=$(docker ps | grep "rest:" | awk '{print $1}')
-
-
 }
 
 teardown_suite() {
   echo -e "\nEnding tests ... \n" >&3
 
-  docker logs $(echo $DOCKER_ID) >&3
-  docker logs $(echo $DOCKER_ID) > ./results/container.log 2>&1
+  docker logs $DOCKER_ID >&3
+  docker logs $DOCKER_ID > ./results/container.log 2>&1
 }
