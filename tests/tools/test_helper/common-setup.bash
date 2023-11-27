eb110d06fb86e8c783e2feb443cbf6a747e3fb27
georgetarazevich <georgetarazevich@users.noreply.github.com>
2023-11-27 20:10:20 +0300

    Update Config option tests with ghcr rest:dev.yml

--- tests/tools/test_helper/common-setup.bash
+++ tests/tools/test_helper/common-setup.bash
@@ -7,7 +7,7 @@ _common_setup() {
     load "$PROJECT_ROOT/bats-support/load.bash"
     load "$PROJECT_ROOT/bats-assert/load.bash"
 
-    export DOCKER_CONTAINER=$(echo $(docker ps | grep "rest:dev" | awk '{print $1}'))
+    export DOCKER_CONTAINER=$(echo $(docker ps | grep "rest:" | awk '{print $1}'))
     echo -e "\nDOCKER_CONTAINER_ID: $DOCKER_CONTAINER \n" >&3
 }
 
