4ae629d8eaf487820c167b26ff8cd78030dbc6d3
Maxim <maxplusim@mail.ru>
2024-02-13 14:51:07 +0300

    Update rest tests

--- tests/Options/services/bindMountLocalFiles/steps_in_docker.sh
+++ tests/Options/services/bindMountLocalFiles/steps_in_docker.sh
@@ -6,5 +6,5 @@ apt-get install curl -y
 cd /home/Res_tmp/
 echo -e "\n"
 
-curl -F "file=@/home/Res_tmp/a.pdf" localhost:8080/api/validate/3a > res_tmp_log.log
+curl -F "file=@/home/Res_tmp/a.pdf" localhost:8080/api/validate/3a > res_tmp_log.log -H 'accept: application/xml'
 exit
