001f245b90ba81eb358d057a8afb917d003bca4b
Maxim <maxplusim@mail.ru>
2025-07-25 18:56:29 +0300

    Add default server with url '/' to swagger

--- src/main/java/org/verapdf/rest/resources/ApiResource.java
+++ src/main/java/org/verapdf/rest/resources/ApiResource.java
@@ -32,7 +32,8 @@ import javax.ws.rs.core.MediaType;
 @Path("/api")
 @Tag(name = "veraPDF")
 @OpenAPIDefinition(info = @Info(title = "veraPDF API", description = "A REST service API for veraPDF", version = "V1.29.1", license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")), servers = {
-        @Server(url = "https://demo.verapdf.org", description = "default"),
+        @Server(url = "/", description = "default"),
+        @Server(url = "https://demo.verapdf.org", description = "prod"),
         @Server(url = "https://dev.verapdf-rest.duallab.com", description = "dev"),
         @Server(url = "http://localhost:8080", description = "local") })
 
